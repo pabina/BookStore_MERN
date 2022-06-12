cd0b503112a0786e194580a235758520c98cf7a2
pabina <rimalpabina1234@gmail.com>
2022-06-12 09:28:08 +0545

    home page added

--- frontend/myapp/src/App.js
+++ frontend/myapp/src/App.js
@@ -1,14 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import AddBook from "./pages/AddBook";
+import HomePage from "./pages/HomePage";
+import Dashboard from "./pages/Dashboard";
+import "./assets/scss/main.scss";
+import { Explore } from "./pages/Explore";
+import ListBook from "./pages/ListBook";
+
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/dashboard">
+          <Route index element={<Dashboard />} />
+          <Route path="addBook" element={<AddBook />} />
+          <Route path="listBook" element={<ListBook />} />
+        </Route>
+        <Route path="*" element={<b>Page Not Found</b>} />
+      </Routes>
+    </Router>
   );
 }
 
